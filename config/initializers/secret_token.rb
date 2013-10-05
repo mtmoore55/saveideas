b61ae2cabab2590eedcf6b23c184c614b35a8cb0
Matthew Moore <mtmoore55@gmail.com>
2013-10-05 11:44:26 -0400

    Created Initial App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Saveideas::Application.config.secret_key_base = '1831f921ef8c84f4f8903ade6318b32b3faa029d2be92611d520263f1a7f2af587e4ed060fd478491f026b98febb9246dbe214bea39d2be04f0cfca84d207f9f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Saveideas::Application.config.session_store :cookie_store, key: '_saveideas_session'
